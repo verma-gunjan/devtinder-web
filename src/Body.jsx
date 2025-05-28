1c5d44a16060411b77f6a0a0cf50ec38088f0bf6
Gunjan Verma <vermagunjan9927@gmail.com>
2025-05-28 19:52:36 +0530

    added footer

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 function Body(){
   return(
     <>
       <NavBar />
       {/* any children route of body will render in outlet */}
       <Outlet />
+      <Footer />
     </>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar=() =>{
   return(
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-neutral shadow-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">DevTinder</a>
         </div>
